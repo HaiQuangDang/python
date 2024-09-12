946155d90e7801b6b47d46dba30bef5f27c9498a
HaiQuangDang <danghaiquang12@gmail.com>
2024-09-12 17:58:05 +0700

    learn format datetime

--- base/datetime/dt.py
+++ base/datetime/dt.py
@@ -1,7 +1,20 @@
 import datetime
-
+import time
 today = datetime.date.today()
 current_time = datetime.datetime.now()
+current_time = current_time.strftime("%m-%d-%Y %H:%M:%S")
 
-print(today)
-print(current_time)
+
+def wake_up():
+    alarm = input("Set your alarm: ")
+    times_up = False
+    while not times_up:
+        current_time = datetime.datetime.now().strftime("%H:%M:%S")
+        print(current_time)
+        if current_time == alarm:
+            print("Times up!")
+            times_up = True
+        time.sleep(1)
+
+
+wake_up()
